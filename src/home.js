8b076cd68b78b8880b2e6677b95440b48400acc7
Sabrina <sabrina.rf2003@gmail.com>
2023-11-25 16:40:05 -0300

    home screen almost finished & cart html created

--- src/home.js
+++ src/home.js
@@ -6,4 +6,35 @@ $(document).ready(function() {
     $("#menu-sanduiche-2").on("click", function() {
         $(".sidebar").css("display", "none");
     });
+
+    const products = [
+        { id: 1, name: "Cama quentinha", tag: "Para doguinhos", image: "../public/assets/heated-dog-bed.jpg", price: 80.00 },
+        { id: 2, name: "Cama quentinha", tag: "Para doguinhos", image: "../public/assets/heated-dog-bed.jpg", price: 80.00 },
+        { id: 3, name: "Cama quentinha", tag: "Para doguinhos", image: "../public/assets/heated-dog-bed.jpg", price: 80.00 },
+        { id: 4, name: "Cama quentinha", tag: "Para doguinhos", image: "../public/assets/heated-dog-bed.jpg", price: 80.00 },
+        { id: 5, name: "Cama quentinha", tag: "Para doguinhos", image: "../public/assets/heated-dog-bed.jpg", price: 80.00 }
+    ];
+
+    function generateHTML(product) {
+        return `
+        <section class="card">
+                <div class="image-container">
+                    <img src="${product.image}" alt="">
+                        <div class="overlay">
+                            <button class="cart-button"><a href="cart.html?product=${product.id}">Adicionar ao carrinho</a></button>
+                        </div>
+                </div>
+                    <div class="card-content">
+                        <h4>${product.name}</h4>
+                        <p class="tag">${product.tag}</p>
+                        <p class="price">R$${product.price.toFixed(2)}</p>
+                    </div>
+        </section>
+    `;
+    }
+
+    let cardsContainer = $(".cards");
+    products.forEach(function (product) {
+        cardsContainer.append(generateHTML(product));
+    });
 });
